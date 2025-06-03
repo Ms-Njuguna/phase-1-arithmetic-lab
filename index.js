953cb19a62c994896c699c92f1b33eea4e99c479
Patricia <patricianjuguna084@gmail.com>
2025-06-03 15:04:22 +0300

    Did the arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+//two numbers multiplying to give 62
+const num1 = 31;
+const num2 = 2;
+
+const multiply = num1 * num2;
+console.log(multiply);
+
+//random integer greater than 0
+const random = Math.floor(Math.random() * 100)  + 1;
+
+console.log(random);
+
+//returning a remainder of 4
+const num3 = 24;
+const num4 = 5;
+
+const mod = num3 % num4;
+
+console.log(mod);
+
+//finds the highest number in a set
+const max= Math.max(12, 20, 7, 2);
+
+console.log(max);
